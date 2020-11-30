ce8cc600ff4ce2b5269c0e9c3d0dc5868d60c92f
Chris Cooley <chriscooley1984@yahoo.com>
2020-11-30 11:57:07 -0700

    added htmlRoutes.js file and updated apiRoute.js with app.post and app.delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,31 @@
+const fs = require("fs");
 var notesData = require("../db/db.json");
+const { v4:uuidv4 } = require("uuid");
 
-modules.exports = function(app) {
-    app.get("/api/notes", function(req, res) {
+module.exports = function(app){
+    app.get("/api/notes", function(req, res){
         res.json(notesData)
-    })
-}
+    });
+
+    app.post("/api/notes", function(req, res){
+        notesData.push({...req.body, id:uuidv4()})
+        fs.writeFile("db/db.json", JSON.stringify(notesData), function(err, log) {
+            if (err){
+                throw err
+            } else {
+                res.json(true)
+            }
+        })
+    });
+
+    app.delete("/api/notes/:id", function(req, res){
+        const filteredNotes = notesData.filter(note => note.id !== req.params.id);
+        fs.writeFile("db/db.json", JSON.stringify(filteredNotes), function(err, log){
+            if (err){
+                throw err
+            } else {
+                res.json(true)
+            }
+        })
+    });
+};
--- server.js
+++ server.js
@@ -1,24 +1,15 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extend: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static ("public"));
+app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
 
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-require("/routes/apiRoutes.js")(app);
-
-app.listen(PORT, function () {
+app.listen(PORT, function (){
     console.log("App listening on PORT " + PORT);
 });
