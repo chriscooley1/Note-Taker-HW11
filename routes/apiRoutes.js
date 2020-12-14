dba87af480e823a51c925633a5373831f7b27de4
Chris Cooley <chriscooley1984@yahoo.com>
2020-12-14 14:53:44 -0700

    added app.post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,40 +9,38 @@ module.exports = function(app){
     app.get("/api/notes", function(req, res){
         readFileAsync("./db/db.json", "utf8").then(notes=>{
             notes = JSON.parse(notes);
-            res.json(notes)
+            res.json(notes);
         })
-        .catch
+        .catch;
     });
 
     app.post("/api/notes", function(req, res){
         readFileAsync("./db/db.json", "utf8").then(notes=>{
             notes = JSON.parse(notes);
-            let updatedNote = {...req.body, id:uuidv4()}
-            notes.push(updatedNote)
-            writeFileAsync("./db/db.json", JSON.stringify(notes))
-        })
+            let updatedNote = {...req.body, id:uuidv4()};
+            notes.push(updatedNote);
+            writeFileAsync("./db/db.json", JSON.stringify(notes));
+        });
 
-        // notesData.push({...req.body, id:uuidv4()})
-        // fs.writeFile("./db/db.json", JSON.stringify(notesData), function(err, log) {
-        //     if (err){
-        //         throw err
-        //     } else {
-        //         res.json(true)
-        //     }
-        // })
+        notesData.push({...req.body, id:uuidv4()});
+        fs.writeFile("./db/db.json", JSON.stringify(notesData), function(err, log) {
+            if (err){
+                throw err
+            } else {
+                res.json(true)
+            }
+        })
     });
 
     app.delete("/api/notes/:id", function(req, res){
         readFileAsync("./db/db.json", "utf8").then(notes=>{
             notes = JSON.parse(notes);
-            console.log(notes);
             let filteredNotes = notes.filter(note => note.id !== req.params.id);
-            // console.log(filteredNotes)
             writeFileAsync("./db/db.json", JSON.stringify(filteredNotes)).then(()=>
             res.json({
                 ok:true
-            }))
+            }));
         })
-        .catch(err => res.status(500).json(err))
+        .catch(err => res.status(500).json(err));
     });
 };
