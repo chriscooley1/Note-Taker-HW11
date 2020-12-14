06f706dec88a654685f845cb3b3b997995330555
Chris Cooley <chriscooley1984@yahoo.com>
2020-12-14 11:10:04 -0700

    JSON.parse and JSON.stringify for notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,30 +3,46 @@ const util = require("util");
 var notesData = require("../db/db.json");
 const { v4:uuidv4 } = require("uuid");
 const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
 
 module.exports = function(app){
     app.get("/api/notes", function(req, res){
-        res.json(notesData)
+        readFileAsync("./db/db.json", "utf8").then(notes=>{
+            notes = JSON.parse(notes);
+            res.json(notes)
+        })
+        .catch
     });
 
     app.post("/api/notes", function(req, res){
-        notesData.push({...req.body, id:uuidv4()})
-        fs.writeFile("db/db.json", JSON.stringify(notesData), function(err, log) {
-            if (err){
-                throw err
-            } else {
-                res.json(true)
-            }
+        readFileAsync("./db/db.json", "utf8").then(notes=>{
+            notes = JSON.parse(notes);
+            let updatedNote = {...req.body, id:uuidv4()}
+            notes.push(updatedNote)
+            writeFileAsync("./db/db.json", JSON.stringify(notes))
         })
+
+        // notesData.push({...req.body, id:uuidv4()})
+        // fs.writeFile("./db/db.json", JSON.stringify(notesData), function(err, log) {
+        //     if (err){
+        //         throw err
+        //     } else {
+        //         res.json(true)
+        //     }
+        // })
     });
 
     app.delete("/api/notes/:id", function(req, res){
-        const filteredNotes = notesData.filter(note => note.id !== req.params.id);
-        console.log(filteredNotes)
-        writeFileAsync("db/db.json", JSON.stringify(filteredNotes)).then(()=>
-        res.json({
-            ok:true
-        }))
+        readFileAsync("./db/db.json", "utf8").then(notes=>{
+            notes = JSON.parse(notes);
+            console.log(notes);
+            let filteredNotes = notes.filter(note => note.id !== req.params.id);
+            // console.log(filteredNotes)
+            writeFileAsync("./db/db.json", JSON.stringify(filteredNotes)).then(()=>
+            res.json({
+                ok:true
+            }))
+        })
         .catch(err => res.status(500).json(err))
     });
 };
